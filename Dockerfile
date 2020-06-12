f6b08ddb4d3c23b5adfa2cc003982b698882ab4d
Javier Cazallas <jcazallasc@gmail.com>
2020-06-12 10:54:44 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.7-alpine
+MAINTAINER Javier Cazallas
+
+ENV PYTHONUNBUFFERED 1
+
+# Install dependencies
+COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev curl
+RUN apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
+
+# Setup directory structure
+RUN mkdir /app
+WORKDIR /app
+COPY ./app/ /app
+
+RUN adduser -D user
+USER user
---
+++ app/users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
